650b3ff10acf9106ea822ab42367a7644007874b
Andre <3oliva41@solent.ac.uk>
2020-02-13 23:07:19 +0000

    added new meta tags and generated favicons from 'favicomatic'

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,49 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+  >
+         {/* Primary Meta Tags */}
+        <title>Boat.world - free automated strategy game</title>
+        <meta name="title" content="Boat.World - free automated strategy game"/>
+        <meta name="description" content="Boat.world is an automated strategy game. Create bots, write scripts, and battle other players! Try it today for free on your computer or mobile device!"/>
+
+         {/* Open Graph / Facebook  */}
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://boat.world/"/>
+        <meta property="og:title" content="Boat.world - free automated strategy game"/>
+        <meta property="og:description" content="Boat.World is an automated strategy game. Create bots, write scripts, and battle other players! Try it today for free on your computer or mobile device!"/>
+        <meta property="og:image" content="https://boat.world/dist/open_graph.png"/>
+
+         {/* Twitter  */}
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://boat.world/"/>
+        <meta property="twitter:title" content="Boat.world - free automated strategy game"/>
+        <meta property="twitter:description" content="Boat.world is an automated strategy game. Create bots, write scripts, and battle other players! Try it today for free on your computer or mobile device!"/>
+        <meta property="twitter:image" content="https://boat.world/dist/open_graph.png"/>
+
+        {/* Mobile */}
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="apple-touch-icon-60x60.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png" />
+        <link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196" />
+        <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128" />
+        <meta name="application-name" content="Boat.world"/>
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+        <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+        <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+        <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+        <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+
+  </Helmet>
   )
 }
 
